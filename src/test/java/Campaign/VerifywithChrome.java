dc3eb5a62aec049d4aafffdcf88b9cca874d1360
hp <hp@LAPTOP-RATHPUUI>
2025-08-12 17:47:48 +0530

    commit

---
+++ src/test/java/Campaign/VerifywithChrome.java
@@ -0,0 +1,13 @@
+package Campaign;
+
+import org.testng.annotations.Test;
+
+public class VerifywithChrome {
+	
+	@Test
+	public void runChrome()
+	{
+		System.out.println("Chrome launched");
+	}
+
+}
